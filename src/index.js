3ba745f10892a262d2dc374cbb8d978cddc13e01
David Umoru <umorudavido@gmail.com>
2024-07-05 15:09:40 +0100

    Set up express app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth.routes");
+const organisationRoutes = require("./routes/organisation.routes");
+const db = require("./models");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
+app.use("/api/organisations", organisationRoutes);
+
+db.sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+});

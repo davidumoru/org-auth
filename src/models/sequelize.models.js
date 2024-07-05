5020a5f0db9554cf378534e4493fb63f7c4cf8df
David Umoru <umorudavido@gmail.com>
2024-07-05 15:21:29 +0100

    Configure sequelize

---
+++ src/models/sequelize.models.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.User = require("./user")(sequelize, Sequelize);
+db.Organisation = require("./organisation")(sequelize, Sequelize);
+
+// Define associations
+db.User.belongsToMany(db.Organisation, { through: "UserOrganisations" });
+db.Organisation.belongsToMany(db.User, { through: "UserOrganisations" });
+
+module.exports = db;

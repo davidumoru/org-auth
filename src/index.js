3f5c59f86d6b7a2b8996c4d9b3887608e7c4f531
David Umoru <umorudavido@gmail.com>
2024-07-07 23:28:25 +0100

    Implement cors middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const organisationRoutes = require("./routes/organisation.routes");
 const db = require("./models/sequelize.models");
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 
